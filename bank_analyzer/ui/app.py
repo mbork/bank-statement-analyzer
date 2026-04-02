20e13b2b612b61eb97d12e3b53fac95da84d6ef3
Marcin Borkowski <mbork@mbork.pl>
2026-04-02 14:45:37 +0200

    Add a "Hello world"

--- bank_analyzer/ui/app.py
+++ bank_analyzer/ui/app.py
@@ -5,7 +5,9 @@ import ttkbootstrap as ttk
 # * Application
 
 class App(ttk.Window):
-        pass
+    def __init__(self) -> None:
+        super().__init__(title="Bank Statement Analyzer")
+        ttk.Label(self, text="Hello World").pack(pady=20)
 
 # * Entry point
 
